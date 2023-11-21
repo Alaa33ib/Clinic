13c017fc3512800260d9edc627de0675de3291ea
Raghad <151539399+RaghadAyed@users.noreply.github.com>
2023-11-21 12:56:00 +0300

    Update PrescribedIn.java

--- PrescribedIn.java
+++ PrescribedIn.java
@@ -1,1 +1,27 @@
-prescribed in
+public class PrescribedIn {
+  
+    private String appointmentID;
+    private String drugID;
+    private String dosage;
+
+    public PrescribedIn(String appID, String dID, String dosage) {
+        appointmentID = appID;
+        drugID = dID;
+        this.dosage = dosage;
+    }
+
+    public void setAppointmentID(String appID) {
+        appointmentID = appID;
+    }
+
+    public void setDrugID(String dID) {
+       drugID = dID;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+    
+
+
+}
